204796c3b5367c842167d5f9b2a35460d1ee5e3b
jim2764 <jim2345678@gmail.com>
2023-01-01 18:34:05 +0800

    你看看 在這裡

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -10,6 +10,9 @@ namespace GitPractice
 	{
 		static void Main(string[] args)
 		{
+			// 阿魚好聰明都看不懂
+			// 這段是我在devlop加上的註解
+
 			Student student = new Student("Yu", true, 45);
 
 			student.MyStudent();
