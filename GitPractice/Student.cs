0e228e40658ce58bde32eeeb709c1ba3bcb6bece
jim2764 <jim2345678@gmail.com>
2023-01-01 14:16:20 +0800

    Add Student Class

---
+++ GitPractice/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPractice
+{
+	public class Student
+	{
+		public Student(string name, bool gender, int grade)
+		{
+			Name = name;
+			Gender = gender;
+			Grade = grade;
+		}
+
+		public string Name { get; set; }
+		public bool Gender { get; set; }
+		public int Grade { get; set; }
+
+		public override string ToString()
+		{
+			string sex = "male";
+			if (!Gender) sex = "female";
+
+			return $"Name: {Name}, Sex:{sex}, Grade:{Grade}";
+		}
+
+		public void MyStudent()
+		{
+			Console.WriteLine(this.ToString());
+		}
+	}
+}
